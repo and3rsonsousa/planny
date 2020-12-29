398c30c8a7d429f1955867cc9410c68dc3ffb22e
Anderson Sousa <and3rsonsousa@outlook.com>
2020-12-29 02:50:49 -0300

    fix purgecss 10

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: {
     content: ["./pages/**/*.js"],
     options: {
-      safelist: ["bg-purple-100", "bg-purple-600"],
+      safelist: [
+        "bg-purple-100",
+        "bg-purple-600",
+        "bg-purple-900",
+        "bg-green-100",
+        "bg-green-600",
+        "bg-green-900",
+        "bg-yellow-100",
+        "bg-yellow-600",
+        "bg-yellow-900",
+        "bg-pink-100",
+        "bg-pink-600",
+        "bg-pink-900",
+      ],
     },
   },
 
